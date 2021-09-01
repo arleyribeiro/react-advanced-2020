1dcbca8ea5f10d28f046774bdb68d39c471a6a33
arleyribeiro <arley.sribeiro@gmail.com>
2021-09-01 18:24:05 -0300

    useRef, preserves value, does not trigger re-render and target DOM nodes/elements

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+  };
+  console.log(refContainer);
+  return (
+    <>
+      <h2>useRef</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+        </div>
+        <div>
+          <input type="text" ref={divContainer} />
+        </div>
+        <button type="submit" className="btn">
+          submit
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default UseRefBasics;
