54797afeadda288a26291306c5c53c2f6abd6c90
arleyribeiro <arley.sribeiro@gmail.com>
2021-09-03 13:01:17 -0300

    UseMemo example

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -6,7 +6,18 @@ import { useFetch } from "../../9-custom-hooks/final/2-useFetch";
 const url = "https://course-api.com/javascript-store-products";
 
 // every time props or state changes, component re-renders
-
+const calculateMostExpensive = (data) => {
+  console.log("calculateMostExpensive");
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price;
+      if (price >= total) {
+        total = price;
+      }
+      return total;
+    }, 0) / 100
+  );
+};
 const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
@@ -16,6 +27,11 @@ const Index = () => {
     setCart(cart + 1);
   }, [cart]);
 
+  const mostExpensive = useMemo(
+    () => calculateMostExpensive(products),
+    [products]
+  );
+
   return (
     <>
       <h1>Count : {count}</h1>
@@ -23,6 +39,7 @@ const Index = () => {
         click me
       </button>
       <h1>cart: {cart}</h1>
+      <h1>Most Expensive: ${mostExpensive}</h1>
       <BigList products={products} addToCart={addToCart} />
     </>
   );
--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url);
     const products = await response.json();
     setData(products);
     setLoading(false);
-  };
+  }, [url]);
 
   useEffect(() => {
     getProducts();
-  }, [url]);
+  }, [url, getProducts]);
   console.log(data);
   return { loading, products: data };
 };
